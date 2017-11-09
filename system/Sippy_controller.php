a3eece0531ab6ce0457c58b7b2a1d61ffff29f14
trafficinc <>
2017-11-09 15:59:10 -0500

    htaccess update

--- system/Sippy_controller.php
+++ system/Sippy_controller.php
@@ -3,34 +3,36 @@
 
 class Sippy_controller {
 
-	public 	  $flash;
-	protected $config;
-	protected $security;
-    	protected $validation;
+    public $flash;
+    protected $config;
+    protected $security;
+    protected $validation;
 
-	public function __construct() {
+    public function __construct()
+    {
 
-		$config = load_config();
-        	$this->config = $config;
-		$this->security = new Security;
-        	$this->flash = new FlashMessages;
-        	$this->validation = new Validation;
-	}
+        $config = load_config();
+        $this->config = $config;
+        $this->security = new Security;
+        $this->flash = new FlashMessages;
+        $this->validation = new Validation;
+    }
 	
 	public function Model($name) {
 		require(APP_DIR .'models/'. $name .'.php');
 		$model = new $name;
 		return $model;
 	}
-	
-	public function View($name, $data = NULL) {
-		if (empty($data)) {
-			$view = new Sippy_view($name);
-		} else {
-			$view = new Sippy_view($name,$data);
-		}
-		return $view;
-	}
+
+    public function View($name, $data = NULL)
+    {
+        if (empty($data)) {
+            $view = new Sippy_view($name);
+        } else {
+            $view = new Sippy_view($name, $data);
+        }
+        return $view;
+    }
 	
 	public function Plugin($name) {
 		require(APP_DIR .'plugins/'. $name .'.php');
